53f7060a0ecdc13fab758bee91d1bffbee4fbfdd
r8m <rlukerin@gmail.com>
2015-11-16 17:59:46 +0300

    update

--- arbSpread.R
+++ arbSpread.R
@@ -1,4 +1,5 @@
 library(dplyr)
+library(data.table)
 
 #Rate depended futures Spread calculation
 getRateSpread<-function(symbols, rate,dateExp,lot=1, sdPeriod=30, daysYear=365){
@@ -20,30 +21,28 @@ getRateSpread<-function(symbols, rate,dateExp,lot=1, sdPeriod=30, daysYear=365){
 
 
 #Stat Arb depended futures Spread calculation
-getStatSpread<-function(symbols,lots, rollperiod=0, smaPeriod=1, daysYear=365){
-    df<-cbind(lots[1]*SMA(Cl(get(symbols[1])),1),lots[2]*SMA(Cl(get(symbols[2])),1))
-    #df<-data.frame(datetime=as.POSIXct(index(df)),as.data.frame(df))
-    colnames(df)<-c("datetime","symbA", "symbB")
+getStatSpread<-function(symbols, rollperiod=0, smaPeriod=1, daysYear=365){
+    symbDT<-cbind(SMA(Cl(get(symbols[1])),smaPeriod),
+                  SMA(Cl(get(symbols[2])),smaPeriod))
     
-    retA=as.numeric(Delt(df$symbA,type="log"))
-    retB=as.numeric(Delt(df$symbB,type="log"))
-    retBeta <- as.numeric(coef(lm(retA ~ retB + 0))[1])
-    retSpread <- retA - retBeta*retB
-    priceModel<-lm(as.numeric(df$symbA) ~ as.numeric(df$symbB))
+    symbDT<-data.table(as.POSIXct(index(symbDT)), symbDT)
+
+    setnames(symbDT,c("datetime","symbA", "symbB"))
+    symbDT<-symbDT[complete.cases(df),]
+    symbDT[,retA:=as.numeric(Delt(symbA,type="log"))]
+    symbDT[,retB:=as.numeric(Delt(symbB,type="log"))]
+    symbDT[,retBeta:=as.numeric(coef(lm(retA ~ retB+0))[1])]
+    symbDT[,retSpread := retA - retBeta*retB]
+    #priceModel<-lm(as.numeric(symbDT$symbA) ~ as.numeric(symbDT$symbB)+0)
+    betaCoef<-as.numeric(lm(as.numeric(symbDT$symbA) ~ as.numeric(symbDT$symbB)+0)[1])
+    betaCoef<-floor(betaCoef)
+    symbDT[,spread:=symbA - betaCoef*symbB]
     
-    rolllm<-rollapply(df,width =100,
-              FUN= function (x)coef(lm(x[,1] ~ x[,2], data=as.data.frame(x))),
-              by.column=FALSE, fill = NULL)
+#     rolllm<-rollapply(df,width =100,
+#               FUN= function (x)coef(lm(x[,1] ~ x[,2], data=as.data.frame(x))),
+#               by.column=FALSE, fill = NULL)
     
-    summary(priceModel)
-    spread <- df$symbA - as.numeric(coef(priceModel)[2])*df$symbB-as.numeric(coef(priceModel)[1])
-    
-    df<-mutate(df, 
-               retspread=retSpread,
-               spread=spread
-               )
-    #df$spreadsd<-c(rep(NA, sdPeriod-1),rollapply(df$spread,sdPeriod, sd, na.rm=TRUE))
-    df
+    #summary(symbDT)
 }
 
 
